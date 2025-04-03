dbfafc983bf265ce3d36c29d2cc5e9e9f138f59c
sherenefaustina <sherenefaustina@gmail.com>
2025-04-03 13:43:35 +0530

    Added my part - Sherene

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    appDir: true, // Ensure Next.js knows you are using the App Router
+  },
 };
 
 export default nextConfig;
+
