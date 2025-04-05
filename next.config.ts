60ff18abaebfd97c622f88ce7b7ed999f8ebf859
Harshith Ashok <harshith@BMBP16.local>
2025-04-05 17:07:06 +0530

    deployment fix

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    appDir: true, // Ensure Next.js knows you are using the App Router
+    // appDir: true, // Ensure Next.js knows you are using the App Router
   },
 };
 
 export default nextConfig;
-
