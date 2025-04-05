548de4e5e85a8c3d3d9fb198fbae911b47e7e92a
Aadhar Kulshrestha <aadharkulshrestha3150@gmail.com>
2025-04-05 14:02:38 +0530

    ADDED ICONS AND UPDATE CSS

--- app/iie-events/page.tsx
+++ app/iie-events/page.tsx
@@ -11,7 +11,7 @@ const Events = () => {
           <h1 className="text-4xl md:text-5xl font-bold text-white mb-4 text-center">
             IIC and Events
           </h1>
-            <div className="w-20 h-1 bg-orange-500 mb-6 mx-auto"></div>
+          <div className="w-20 h-1 bg-orange-500 mb-6 mx-auto"></div>
           <p className="text-white max-w-4xl text-lg md:text-xl mx-auto text-center mb-8">
             The Institution's Innovation Council (IIC) at the IIE Centre is a
             dynamic hub dedicated to fostering an entrepreneurial mindset,
@@ -51,7 +51,20 @@ const Events = () => {
               <div className="p-6">
                 <div className="flex items-start gap-6">
                   <div className="bg-orange-500/10 p-4 rounded-full">
-                    {/* <Calendar className="h-8 w-8 text-orange-500" /> */}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="h-8 w-8 text-orange-500"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5"
+                      />
+                    </svg>
                   </div>
                   <div>
                     <h3 className="text-2xl font-bold mb-4 text-gray-800">
@@ -88,7 +101,20 @@ const Events = () => {
               <div className="p-6">
                 <div className="flex items-start gap-6">
                   <div className="bg-blue-500/10 p-4 rounded-full">
-                    {/* <Briefcase className="h-8 w-8 text-blue-500" /> */}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="h-8 w-8 text-blue-500"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
+                      />
+                    </svg>
                   </div>
                   <div>
                     <h3 className="text-2xl font-bold mb-4 text-gray-800  ">
@@ -124,10 +150,25 @@ const Events = () => {
               <div className="p-6">
                 <div className="flex items-start gap-6">
                   <div className="bg-green-500/10 p-4 rounded-full">
-                    {/* <Users className="h-8 w-8 text-green-500" /> */}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="h-8 w-8 text-green-500"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+                      />
+                    </svg>
                   </div>
                   <div>
-                    <h3 className="text-2xl font-bold mb-4 text-gray-800">Townhall </h3>
+                    <h3 className="text-2xl font-bold mb-4 text-gray-800">
+                      Townhall{" "}
+                    </h3>
                     <p className="text-gray-700 mb-6">
                       The Townhall is a vibrant, campus-wide gathering that
                       serves as a nexus for exploration, collaboration, and
--- app/mentors/page.tsx
+++ app/mentors/page.tsx
@@ -60,7 +60,9 @@ const Mentors = () => {
       {/* Hero Section */}
       <div className="bg-black text-white">
         <div className="container mx-auto py-28 px-4">
-          <h1 className="text-5xl md:text-6xl font-bold mb-6 text-center">Our Mentors</h1>
+          <h1 className="text-5xl md:text-6xl font-bold mb-6 text-center">
+            Our Mentors
+          </h1>
           <div className="w-20 h-1 bg-orange-500 mb-6 mx-auto"></div>
           <p className="text-xl md:text-2xl max-w-3xl mx-auto text-center">
             Our mentors are seasoned professionals from diverse industries,
@@ -88,7 +90,22 @@ const Mentors = () => {
 
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             <Card className="p-6 border-none shadow-md flex flex-col items-center text-center">
-              <div className="h-12 w-12 rounded-full bg-orange-500 flex items-center justify-center mb-4"></div>
+              <div className="h-12 w-12 rounded-full bg-orange-500 flex items-center justify-center mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-6 w-6 text-white"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+              </div>
               <h3 className="text-xl font-semibold mb-2  text-black">
                 Industry Experience
               </h3>
@@ -99,8 +116,25 @@ const Mentors = () => {
             </Card>
 
             <Card className="p-6 border-none shadow-md flex flex-col items-center text-center">
-              <div className="h-12 w-12 rounded-full bg-orange-500 flex items-center justify-center mb-4"></div>
-              <h3 className="text-xl font-semibold mb-2  text-black">Strategic Guidance</h3>
+              <div className="h-12 w-12 rounded-full bg-orange-500 flex items-center justify-center mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-6 w-6 text-white"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
+                  />
+                </svg>
+              </div>
+              <h3 className="text-xl font-semibold mb-2  text-black">
+                Strategic Guidance
+              </h3>
               <p className="text-gray-600">
                 Personalized advice to overcome challenges and identify
                 opportunities for growth and innovation.
@@ -108,8 +142,25 @@ const Mentors = () => {
             </Card>
 
             <Card className="p-6 border-none shadow-md flex flex-col items-center text-center">
-              <div className="h-12 w-12 rounded-full bg-orange-500 flex items-center justify-center mb-4"></div>
-              <h3 className="text-xl font-semibold mb-2  text-black">Network Access</h3>
+              <div className="h-12 w-12 rounded-full bg-orange-500 flex items-center justify-center mb-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-6 w-6 text-white"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
+                  />
+                </svg>
+              </div>
+              <h3 className="text-xl font-semibold mb-2  text-black">
+                Network Access
+              </h3>
               <p className="text-gray-600">
                 Connect with influential industry leaders and potential
                 partners, investors, and collaborators.
